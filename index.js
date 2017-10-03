f28f8a434b330fa6f5efe4f9053b50845397b276
Cedric Niguidula <cedcn-9mail@yahoo.com>
2017-10-03 04:02:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function updateObjectWithKeyAndValue(objecta, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+<<<<<<< HEAD
   object[key] = value
   return object
 }
@@ -20,3 +21,8 @@ function destructivelyDeleteFromObjectByKey(object, key) {
     delete object[key]
     return object
 }
+=======
+  object.key=value
+  return object
+}
+>>>>>>> 6643e3a025bbd84623998535957c9c37f3218a7c
